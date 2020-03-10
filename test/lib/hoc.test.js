cbaeb3c03c112fa849f6555b98faaf7d1d21e4be
CarlosVazPI <carlos.vazpi@gmail.com>
2020-03-10 21:16:56 +0000

    Parser works

--- test/lib/hoc.test.js
+++ test/lib/hoc.test.js
@@ -1,129 +1,147 @@
 const assert = require('assert')
 const {
-	MATCH,
-	LITERAL,
-	CONCAT,
-	DISJUNCTION,
-	OPTION,
-	CLOSURE,
-	REPETITION,
-	EXCEPTION,
-	EXPECT
+  MATCH,
+  LITERAL,
+  CONCAT,
+  DISJUNCTION,
+  OPTION,
+  CLOSURE,
+  REPETITION,
+  EXCEPTION,
+  EXPECT
 } = require('../../lib/hoc')
 
 describe('hoc', () => {
-	const input = 'this is a text'
+  const input = 'this is a text'
 
-	it('tests LITERAL positive', () => {
-		assert.deepEqual(LITERAL('this')(input), [['LITERAL', 'this'], ' is a text'])
-	})
-	it('tests LITERAL negative', () => {
-		assert.deepEqual(LITERAL('that')(input), [['LITERAL', null], 'this is a text'])
-	})
+  describe('LITERAL', () => {
+    it('tests LITERAL positive', () => {
+      assert.deepEqual(LITERAL('this')(input), [['LITERAL', 'this'], ' is a text'])
+    })
+    it('tests LITERAL negative', () => {
+      assert.deepEqual(LITERAL('that')(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests MATCH positive', () => {
-		assert.deepEqual(MATCH(/\w+/)(input), [['MATCH', 'this'], ' is a text'])
-	})
-	it('tests MATCH negative', () => {
-		assert.deepEqual(MATCH(/[q]h?i\w/)(input), [['MATCH', null], 'this is a text'])
-	})
+  describe('MATCH', () => {
+    it('tests MATCH positive', () => {
+      assert.deepEqual(MATCH(/\w+/)(input), [['MATCH', 'this'], ' is a text'])
+    })
+    it('tests MATCH negative', () => {
+      assert.deepEqual(MATCH(/[q]h?i\w/)(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests CONCAT positive', () => {
-		assert.deepEqual(CONCAT(
-			LITERAL('this'),
-			MATCH(/[ \t\n]/),
-			LITERAL('is'),
-		)(input), [['CONCAT', [['LITERAL', 'this'], ['MATCH', ' '], ['LITERAL', 'is']]], ' a text'])
-	})
-	it('tests CONCAT negative', () => {
-		assert.deepEqual(CONCAT(
-			LITERAL('this'),
-			MATCH(/[ \t\n]/),
-			LITERAL('was'),
-		)(input), [['CONCAT', null], 'this is a text'])
-	})
+  describe('CONCAT', () => {
+    it('tests CONCAT positive', () => {
+      assert.deepEqual(CONCAT(
+        LITERAL('this'),
+        MATCH(/[ \t\n]/),
+        LITERAL('is'),
+      )(input), [['LIST', [['LITERAL', 'this'], ['MATCH', ' '], ['LITERAL', 'is']]], ' a text'])
+    })
+    it('tests CONCAT negative', () => {
+      assert.deepEqual(CONCAT(
+        LITERAL('this'),
+        MATCH(/[ \t\n]/),
+        LITERAL('was'),
+      )(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests DISJUNCTION positive', () => {
-		assert.deepEqual(DISJUNCTION(
-			LITERAL('this'),
-			LITERAL('that'),
-		)(input), [['DISJUNCTION', ['LITERAL', 'this']], ' is a text'])
-	})
-	it('tests DISJUNCTION negative', () => {
-		assert.deepEqual(DISJUNCTION(
-			LITERAL('these'),
-			LITERAL('that'),
-		)(input), [['DISJUNCTION', null], 'this is a text'])
-	})
+  describe('DISJUNCTION', () => {
+    it('tests DISJUNCTION positive', () => {
+      assert.deepEqual(DISJUNCTION(
+        LITERAL('this'),
+        LITERAL('that'),
+      )(input), [['LITERAL', 'this'], ' is a text'])
+    })
+    it('tests DISJUNCTION negative', () => {
+      assert.deepEqual(DISJUNCTION(
+        LITERAL('these'),
+        LITERAL('that'),
+      )(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests OPTION positive', () => {
-		assert.deepEqual(OPTION(
-			LITERAL('this')
-		)(input), [['OPTION', ['LITERAL', 'this']], ' is a text'])
-	})
-	it('tests OPTION negative', () => {
-		assert.deepEqual(OPTION(
-			LITERAL('that')
-		)(input), [['OPTION', ''], 'this is a text'])
-	})
+  describe('OPTION', () => {
+    it('tests OPTION positive', () => {
+      assert.deepEqual(OPTION(
+        LITERAL('this')
+      )(input), [['LITERAL', 'this'], ' is a text'])
+    })
+    it('tests OPTION negative', () => {
+      assert.deepEqual(OPTION(
+        LITERAL('that')
+      )(input), [['LIST', []], 'this is a text'])
+    })
+  })
 
-	it('tests CLOSURE positive', () => {
-		assert.deepEqual(CLOSURE(
-			MATCH(/\w/)
-		)(input), [['CLOSURE', [['MATCH', 't'],['MATCH', 'h'],['MATCH', 'i'],['MATCH', 's']]], ' is a text'])
-	})
-	it('tests CLOSURE negative', () => {
-		assert.deepEqual(CLOSURE(
-			MATCH('\W')
-		)(input), [['CLOSURE', []], 'this is a text'])
-	})
+  describe('CLOSURE', () => {
+    it('tests CLOSURE positive', () => {
+      assert.deepEqual(CLOSURE(
+        MATCH(/\w/)
+      )(input), [['LIST', [['MATCH', 't'],['MATCH', 'h'],['MATCH', 'i'],['MATCH', 's']]], ' is a text'])
+    })
+    it('tests CLOSURE negative', () => {
+      assert.deepEqual(CLOSURE(
+        MATCH('\W')
+      )(input), [['LIST', []], 'this is a text'])
+    })
+  })
 
-	it('tests REPETITION positive', () => {
-		assert.deepEqual(REPETITION(
-			4, MATCH(/\w/)
-		)(input), [['REPETITION', [['MATCH', 't'],['MATCH', 'h'],['MATCH', 'i'],['MATCH', 's']]], ' is a text'])
-	})
-	it('tests REPETITION negative', () => {
-		assert.deepEqual(REPETITION(
-			5, MATCH('\W')
-		)(input), [['REPETITION', null], 'this is a text'])
-	})
+  describe('REPETITION', () => {
+    it('tests REPETITION positive', () => {
+      assert.deepEqual(REPETITION(
+        4, MATCH(/\w/)
+      )(input), [['LIST', [['MATCH', 't'],['MATCH', 'h'],['MATCH', 'i'],['MATCH', 's']]], ' is a text'])
+    })
+    it('tests REPETITION negative', () => {
+      assert.deepEqual(REPETITION(
+        5, MATCH('\W')
+      )(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests EXCEPTION positive', () => {
-		assert.deepEqual(EXCEPTION(
-			MATCH(/\w+/), LITERAL('that')
-		)(input), [['EXCEPTION', 'this'], ' is a text'])
-	})
-	it('tests EXCEPTION negative', () => {
-		assert.deepEqual(EXCEPTION(
-			MATCH(/\w+/), LITERAL('this')
-		)(input), [['EXCEPTION', null], 'this is a text'])
-	})
+  describe('EXCEPTION', () => {
+    it('tests EXCEPTION positive', () => {
+      assert.deepEqual(EXCEPTION(
+        MATCH(/\w+/), LITERAL('that')
+      )(input), [['MATCH', 'this'], ' is a text'])
+    })
+    it('tests EXCEPTION negative', () => {
+      assert.deepEqual(EXCEPTION(
+        MATCH(/\w+/), LITERAL('this')
+      )(input), [[null], 'this is a text'])
+    })
+  })
 
-	it('tests EXPECT positive', () => {
-		const errors = []
-		assert.deepEqual(EXPECT(
-			LITERAL('this'),
-			'this'
-		)(input, { original: input, errors }), [['LITERAL', 'this'], ' is a text'])
-		assert.deepEqual(errors, [])
-	})
-	it('tests EXPECT negative', () => {
-		const errors = []
-		assert.deepEqual(EXPECT(
-			LITERAL('that'),
-			'that',
-		)(input, { original: input, errors }), [['EXPECT', null], 'this is a text'])
-		assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'"])
-		assert.deepEqual(EXPECT(
-			LITERAL('that'),
-			'that',
-		)(input, { original: 'original text\nwith some lines\nand then this is a text', errors }), [['EXPECT', null], 'this is a text'])
-		assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'", "✘ 3:9 | Expected 'that', got 'this is a text...'"])
-		assert.deepEqual(EXPECT(
-			LITERAL('Something'),
-			'Something',
-		)('', { original: 'original text\nwith some lines\nand then this is a text', errors }), [['EXPECT', null], ''])
-		assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'", "✘ 3:9 | Expected 'that', got 'this is a text...'", "✘ 3:23 | Expected 'Something', got end of input"])
-	})
+  describe('EXPCET', () => {
+    it('tests EXPECT positive', () => {
+      const errors = []
+      assert.deepEqual(EXPECT(
+        LITERAL('this'),
+        'this'
+      )(input, { original: input, errors }), [['LITERAL', 'this'], ' is a text'])
+      assert.deepEqual(errors, [])
+    })
+    it('tests EXPECT negative', () => {
+      const errors = []
+      assert.deepEqual(EXPECT(
+        LITERAL('that'),
+        'that',
+      )(input, { original: input, errors }), [[null], 'this is a text'])
+      assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'"])
+      assert.deepEqual(EXPECT(
+        LITERAL('that'),
+        'that',
+      )(input, { original: 'original text\nwith some lines\nand then this is a text', errors }), [[null], 'this is a text'])
+      assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'", "✘ 3:9 | Expected 'that', got 'this is a text...'"])
+      assert.deepEqual(EXPECT(
+        LITERAL('Something'),
+        'Something',
+      )('', { original: 'original text\nwith some lines\nand then this is a text', errors }), [[null], ''])
+      assert.deepEqual(errors, ["✘ 1:0 | Expected 'that', got 'this is a text...'", "✘ 3:9 | Expected 'that', got 'this is a text...'", "✘ 3:23 | Expected 'Something', got end of input"])
+    })
+  })
 })
