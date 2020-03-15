16e35a2e089fcb29a81ff59c024436b466db639f
CarlosVazPI <carlos.vazpi@gmail.com>
2020-03-15 21:12:34 +0000

    Added test

--- test/lib/parser.test.js
+++ test/lib/parser.test.js
@@ -22,7 +22,7 @@ describe('parser module', () => {
     const tree = sParser.S('-.31 + 3.41 * 2 / (1+4)')
     assert.deepEqual(tree, ["S",["$CONCAT",["F",["$CONCAT",["B",["$CONCAT",["N",["$CONCAT",["$MATCH","-.31"],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]]]]]],["$CONCAT"]]],["$CONCAT",["$CONCAT",["$LITERAL","+"]],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]],["S",["$CONCAT",["F",["$CONCAT",["B",["$CONCAT",["N",["$CONCAT",["$MATCH","3.41",".41"],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]]]]]],["$CONCAT",["$CONCAT",["$LITERAL","*"]],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]],["F",["$CONCAT",["B",["$CONCAT",["N",["$CONCAT",["$MATCH","2",undefined],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]]]]]],["$CONCAT",["$CONCAT",["$LITERAL","/"]],["Ws",["$CONCAT",["$CONCAT",["$CONCAT",["$MATCH"," "]]]]],["F",["$CONCAT",["B",["$CONCAT",["$LITERAL","("],["Ws",["$CONCAT",["$CONCAT"]]],["S",["$CONCAT",["F",["$CONCAT",["B",["$CONCAT",["N",["$CONCAT",["$MATCH","1+4","+4"],["Ws",["$CONCAT",["$CONCAT"]]]]]]],["$CONCAT"]]],["$CONCAT"]]],["$LITERAL",")"],["Ws",["$CONCAT",["$CONCAT"]]]]],["$CONCAT"]]]]]]]]],["$CONCAT"]]]]]])
   })
-  it('Analyzes a programming language grammar', () => {
+  it('Analyzes a programming language grammar with grammar errors', () => {
     const tree = parser(`
       Program = [W] { Declaration };
       Declaration = 'export' W Definition ['as' W Id] ['from' W IdPath] | Definition;
@@ -38,5 +38,23 @@ describe('parser module', () => {
 export type MyType
     `)
     assert.deepEqual(tree, ["Program",["$CONCAT",["$CONCAT",["W",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH","\n"]]],["$CONCAT"]]]],["$CONCAT"]]])
+  })
+  it('Analyzes a programming language grammar with program errors', () => {
+    const tree = parser(`
+      Program = [W] { Declaration };
+      Declaration = 'export' W Definition ['as' W Id] ['from' W IdPath] | Definition;
+      Definition = 'type' W CapitalId ['=' [W] Type];
+      CapitalId = /[A-Z][a-zA-Z0-9_]*/ [W];
+      Type = CapitalId { CapitalId };
+      Id = /[a-zA-Z_][a-zA-Z0-9_]*/ - Reserved [W];
+      IdPath = Id { '::' Id };
+      W = Whitespace { Whitespace };
+      Whitespace = /[ \n\t]/;
+      Reserved = 'type' | 'function' | 'module' | 'as' | 'from' | 'export' | 'import' | 'of' | 'in' | 'let' | 'private';
+    `).Program(`
+export type; MyType
+    `)
+    console.log(JSON.stringify(tree))
+    assert.deepEqual(tree, ["Program",["$CONCAT",["$CONCAT",["W",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH","\n"]]],["$CONCAT"]]]],["$CONCAT",["$CONCAT",["Declaration",["$CONCAT",["$LITERAL","export"],["W",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]],["$CONCAT"]]],["Definition",["$CONCAT",["$LITERAL","type"],["W",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]],["$CONCAT"]]],["CapitalId",["$CONCAT",["$MATCH","MyType"],["$CONCAT",["W",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH","\n"]]],["$CONCAT",["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]]],["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]]],["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]]],["$CONCAT",["Whitespace",["$CONCAT",["$MATCH"," "]]]]]]]]]],["$CONCAT"]]],["$CONCAT"],["$CONCAT"]]]]]]])
   })
 })
